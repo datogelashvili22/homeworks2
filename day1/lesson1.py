573c6cdad9f735800fd47900f8b6ab1d3f33cbfb
datogelashvili22 <davitbrown202@gmail.com>
2023-05-15 16:26:54 +0400

    day1 and day2

---
+++ day1/lesson1.py
@@ -0,0 +1,5 @@
+name = "Daviti"
+surname = "Gelashvili"
+age = "22"
+height = 165.2
+print("Me Var" + " " + str(age) + " wlis" + "Chemi Saxelia" + " " + name + " " + "Gvarad" + surname + " " + "simagle " + " " + str(height) )
