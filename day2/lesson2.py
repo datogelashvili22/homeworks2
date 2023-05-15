ba8e6bde24311bc7e0ec105140da6158a6f53b26
datogelashvili22 <davitbrown202@gmail.com>
2023-05-15 18:34:43 +0400

    day2 commit

--- day2/lesson2.py
+++ day2/lesson2.py
@@ -1,1 +1,42 @@
-print("this is a lesson 2")
+#name = "Davit"
+
+#print(len(name))
+#5
+
+#my_sentence = """გამარჯობა შეგახსენებთ რომ სწავლას
+#იწყებთ 19 
+#სექტემბერს, გთხოვთ არ გააცდინოთ"""
+
+#print(my_sentence)
+
+know_programing = True
+
+#if True: 
+ #  print("hello")
+
+
+#full_name = "Davit Gelashvili"
+
+#print(len(full_name))
+
+#print(full_name[9])
+
+#print("D" not in full_name)
+
+full_name = "DAVIT GELASHVILI"
+# print(full_name[2:9])
+# print(full_name[2:9:2])
+
+# print(full_name[:5])
+# print(full_name[6:])
+
+# print(full_name[-1:-6:-1])
+
+print(full_name.upper())
+
+name2 = "Davit Gelashvili"
+# print(name2.lower())
+
+# print(name2.strip())
+
+print(name2.replace("a", "$"))
